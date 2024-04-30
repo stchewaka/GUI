aed31d4fae5bd70f65c779ebf11aa80e92b345e2
Simon Chewaka <57150728+stchewaka@users.noreply.github.com>
2024-04-30 16:11:06 -0500

    Add files via upload

---
+++ Sprint4/Assignment5/src/guiguy/Sprint.java
@@ -0,0 +1,408 @@
+package guiguy;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
+public class Sprint {
+    private static ButtonGroup buttonGroupRed;
+    private static ButtonGroup buttonGroupBlue;
+    private static JRadioButton horizontalRadioButton1;
+    private static JRadioButton horizontalRadioButton2;
+    private static boolean isRedSClicked = false; 
+    private static boolean isRedOClicked = false; 
+    private static boolean isBlueSClicked = false; 
+    private static boolean isBlueOClicked = false; 
+    private static int boardSize = 7; 
+    private static boolean isRedTurn = true; 
+    private static JLabel turnLabel; 
+    private static int redScore = 0;
+    private static int blueScore = 0;
+    private static String[][] board;
+    private static JPanel gameBoardPanel;
+    private static String computerSymbol;
+    
+    
+    public static void main(String[] args) {
+    	String[] options = {"Singleplayer (You are Red)", "Singleplayer (You are Blue)", "Multiplayer"};
+        int choice = JOptionPane.showOptionDialog(null, "Select Game Mode", "Game Mode",
+                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+
+        // Set up variables to track game mode
+        final boolean isSinglePlayer = (choice == JOptionPane.YES_OPTION || choice == JOptionPane.NO_OPTION);
+        boolean isYouRed = (choice == JOptionPane.YES_OPTION);
+        
+        JFrame frame = new JFrame("SOS Game");
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+        JLabel blueLabel = new JLabel("Blue Player");
+        leftPanel.add(Box.createVerticalGlue());
+        leftPanel.add(blueLabel);
+        leftPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        //blue button group
+        buttonGroupBlue = new ButtonGroup();
+        JRadioButton radioButtonBlue1 = new JRadioButton("S");
+        JRadioButton radioButtonBlue2 = new JRadioButton("O");
+
+        buttonGroupBlue.add(radioButtonBlue1);
+        buttonGroupBlue.add(radioButtonBlue2);
+
+        // makes the buttons stay in the middle of the right side of the screen
+        leftPanel.add(radioButtonBlue1);
+        leftPanel.add(radioButtonBlue2);
+        leftPanel.add(Box.createVerticalGlue());
+
+        JCheckBox checkBox = new JCheckBox("Record??");
+        leftPanel.add(checkBox);;
+        
+        
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BorderLayout());
+
+        
+        JPanel horizontalRadioButtonsPanel = new JPanel();
+        horizontalRadioButtonsPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        // creates label for the title of the game
+        JLabel label = new JLabel("SOS Game");
+        horizontalRadioButtonsPanel.add(label);
+
+        horizontalRadioButton1 = new JRadioButton("Simple Game");
+        horizontalRadioButton2 = new JRadioButton("General Game");
+
+        ButtonGroup horizontalButtonGroup = new ButtonGroup();
+        horizontalButtonGroup.add(horizontalRadioButton1);
+        horizontalButtonGroup.add(horizontalRadioButton2);
+
+        horizontalRadioButtonsPanel.add(horizontalRadioButton1);
+        horizontalRadioButtonsPanel.add(horizontalRadioButton2);
+
+        //radio buttons on the right of the screen
+        JPanel radioButtonsPanel = new JPanel();
+        radioButtonsPanel.setLayout(new BoxLayout(radioButtonsPanel, BoxLayout.Y_AXIS));
+
+        //board size adjuster
+        JLabel boardSizeLabel = new JLabel("Board Size");
+        radioButtonsPanel.add(boardSizeLabel);
+
+        //buttons for red player
+        JLabel redLabel = new JLabel("Red Player");
+        radioButtonsPanel.add(Box.createVerticalGlue());
+        radioButtonsPanel.add(redLabel);
+        radioButtonsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        buttonGroupRed = new ButtonGroup(); // Changed from local variable to class variable
+        JRadioButton radioButtonRed1 = new JRadioButton("S");
+        JRadioButton radioButtonRed2 = new JRadioButton("O");
+
+        buttonGroupRed.add(radioButtonRed1);
+        buttonGroupRed.add(radioButtonRed2);
+
+        //action listeners to radio buttons for red player
+        radioButtonRed1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isRedSClicked = true;
+                isRedOClicked = false;
+            }
+        });
+
+        radioButtonRed2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isRedSClicked = false;
+                isRedOClicked = true;
+            }
+        });
+     // action listeners to radio buttons for blue player
+        radioButtonBlue1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isBlueSClicked = true;
+                isBlueOClicked = false;
+            }
+        });
+
+        radioButtonBlue2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isBlueSClicked = false;
+                isBlueOClicked = true;
+            }
+        });
+
+        
+        radioButtonsPanel.add(radioButtonRed1);
+        radioButtonsPanel.add(radioButtonRed2);
+        radioButtonsPanel.add(Box.createVerticalGlue());
+
+        //board size
+        final JPanel gameBoardPanel = new JPanel();
+        updateGameBoard(gameBoardPanel, boardSize, isSinglePlayer); // Initialize the game board
+
+        //board size adjuster
+        final JSpinner boardSizeSpinner = new JSpinner();
+        boardSizeSpinner.setModel(new SpinnerNumberModel(boardSize, 3, 8, 1)); //default, minimum, and maximum
+        boardSizeSpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                boardSize = (int) boardSizeSpinner.getValue();
+                updateGameBoard(gameBoardPanel, boardSize, isSinglePlayer);
+            }
+        });
+        radioButtonsPanel.add(boardSizeSpinner);
+
+       
+        turnLabel = new JLabel("Red Player's Turn");
+        rightPanel.add(turnLabel, BorderLayout.SOUTH); // Add the label to the bottom of rightPanel
+
+        rightPanel.add(radioButtonsPanel, BorderLayout.CENTER);
+        rightPanel.add(gameBoardPanel, BorderLayout.CENTER);
+        rightPanel.add(horizontalRadioButtonsPanel, BorderLayout.NORTH);
+
+        // makes the panels move to the respective side of the screen
+        JPanel mainPanel = new JPanel();
+        mainPanel.add(leftPanel, BorderLayout.WEST);
+        mainPanel.add(rightPanel, BorderLayout.CENTER);
+        mainPanel.add(radioButtonsPanel, BorderLayout.EAST);
+
+        JPanel bottomLeftPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        bottomLeftPanel.add(checkBox);
+
+        JPanel containerPanel = new JPanel(new BorderLayout());
+        containerPanel.add(mainPanel, BorderLayout.CENTER);
+        containerPanel.add(bottomLeftPanel, BorderLayout.SOUTH);
+
+        frame.getContentPane().add(containerPanel);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton newGameButton = new JButton ("New Game");
+        newGameButton.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		clearGameBoard(gameBoardPanel, boardSize);
+        	}
+        });
+        leftPanel.add(newGameButton);
+        
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+        
+        if (isSinglePlayer) {
+            board = new String[boardSize][boardSize];
+            if (!isYouRed) {
+               
+                isRedTurn = false;
+                computerMove();
+            }
+        }
+    }	
+    private static void computerMove() {
+        if (!isRedTurn) {
+            //AI for the computer's move
+            Random random = new Random();
+            int row, col;
+            do {
+                row = random.nextInt(boardSize);
+                col = random.nextInt(boardSize);
+            } while (board[row][col] != null);
+
+            
+            simulateButtonClick(row, col);
+
+            // Check for a win condition after the computer's move
+            if (checkForWin(board, row, col)) {
+                turnLabel.setText("you win");
+                disableButtons(gameBoardPanel);
+            }
+        }
+    }
+    private static void simulateButtonClick(int row, int col) {
+        // Find the button at the specified position and trigger its ActionListener
+        Component[] buttons = gameBoardPanel.getComponents();
+        for (Component button : buttons) {
+            if (button instanceof JButton) {
+                JButton jButton = (JButton) button;
+                int buttonRow = (int) jButton.getClientProperty("row");
+                int buttonCol = (int) jButton.getClientProperty("col");
+                if (buttonRow == row && buttonCol == col) {
+                    jButton.doClick();
+                    break;
+                }
+            }
+        }
+    }
+
+ // Method to clear the game board
+    private static void clearGameBoard(JPanel gameBoardPanel, int size) {
+        Component[] buttons = gameBoardPanel.getComponents();
+        for (Component button : buttons) {
+            if (button instanceof JButton) {
+                JButton jButton = (JButton) button;
+                jButton.setText("");
+                jButton.setForeground(Color.BLACK); // Reset button color
+            }
+        }
+        isRedTurn = true; 
+        turnLabel.setText("Red Player's Turn");
+    }
+ //check for a draw condition
+    private static boolean checkForDraw(String[][] board) {
+        for (String[] row : board) {
+            for (String cell : row) {
+                if (cell == null || cell.isEmpty()) {
+                   
+                    return false;
+                }
+            }
+        }
+        // All cells are filled with "S" or "O," and no win condition is satisfied
+        return true;
+    }
+    private static void updateGameBoard(final JPanel gameBoardPanel, int size, final boolean isSinglePlayer) {
+        gameBoardPanel.removeAll(); // Clear previous components
+        gameBoardPanel.setLayout(new GridLayout(size, size)); // Set new grid layout
+
+        int cellSize = 50;
+
+        // Initialize the board array
+        board = new String[size][size];	
+        
+        Sprint.gameBoardPanel = gameBoardPanel;
+        
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                final JButton button = new JButton();
+                button.setPreferredSize(new Dimension(cellSize, cellSize));
+                button.setBackground(Color.WHITE);
+                button.setBorder(BorderFactory.createLineBorder(Color.black));
+                gameBoardPanel.add(button);
+
+                final int row = i;
+                final int col = j;
+                
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (isRedTurn) {
+                        	button.setForeground(Color.RED);
+                            if (isRedSClicked) {
+                                button.setText("S");
+                                board[row][col] = "S";
+                            } else if (isRedOClicked) {
+                                button.setText("O");
+                                board[row][col] = "O";
+                            }
+                            turnLabel.setText("Blue Player's Turn"); // Set label text for Blue Player's Turn
+                            if (checkForWin(board,row,col)) {
+                            	redScore++;
+                            }
+                        } else {
+                        	button.setForeground(Color.BLUE);
+                            if (isBlueSClicked) {
+                                button.setText("S");
+                                board[row][col] = "S";
+                            } else if (isBlueOClicked) {
+                                button.setText("O");
+                                board[row][col] = "O";
+                            }
+                            turnLabel.setText("Red Player's Turn"); // Set label text for Red Player's Turn
+                            if (checkForWin(board,row,col)) {
+                            	blueScore++;
+                            }
+                        }
+                        isRedTurn = !isRedTurn; // Switch turn
+                        
+                        //check for win condition
+                        if (horizontalRadioButton1.isSelected()) {
+                        	if (checkForWin(board, row, col)) {
+                            	turnLabel.setText("Blue is the Winner");
+                            	disableButtons(gameBoardPanel);
+                            } else if (checkForDraw(board)) {
+                            	turnLabel.setText("Draw");
+                            	disableButtons(gameBoardPanel);
+                            }
+                        }
+                        if (horizontalRadioButton2.isSelected()) {
+                        	if (isBoardFull(board)) {
+                            	determineWinner();
+                            	disableButtons(gameBoardPanel);
+                            }
+                        }
+                        if (isSinglePlayer && !checkForWin(board, row, col) && !checkForDraw(board)) {
+                            computerMove();
+                        }
+                    }
+                });
+                
+                button.putClientProperty("row", i);
+                button.putClientProperty("col", j);
+            }
+        }
+
+        gameBoardPanel.revalidate();
+        gameBoardPanel.repaint();
+    }
+//check if board is full
+private static boolean isBoardFull(String[][] board) {
+	for (String [] row : board) {
+		for (String cell : row) {
+			if (cell == null || cell.isEmpty()) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+private static void determineWinner() {
+	if (redScore > blueScore) {
+		turnLabel.setText("Red Player Wins");
+	} else if (redScore < blueScore) {
+		turnLabel.setText("Blue Player Wins");
+	} else {
+		turnLabel.setText("Draw");
+	}
+}
+
+//method for checking win condition
+private static boolean checkForWin(String[][] board, int row, int col) {
+	String[] sos = {"S", "O", "S"};
+	for (int i = -1; i <= 1; i++) {
+		for (int j = -1; j <= 1; j++) {
+			if (i == 0 && j == 0) {
+				continue;
+			}
+			int count = 0;
+			for (int k =0; k < 3; k++) {
+				int newRow = row + i * k;
+				int newCol = col + j * k;
+				if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length && board [newRow][newCol] != null && board[newRow][newCol].equals(sos[k])) {
+					count++;
+				}
+			}
+			if (count == 3) {
+				return true; // found win condition
+			}
+		}
+	}
+	return false;
+}
+
+// method to disable all buttons after a win
+private static void disableButtons(JPanel gameBoardPanel) {
+	Component[] buttons = gameBoardPanel.getComponents();
+	for (Component button : buttons) {
+		if (button instanceof JButton) {
+			JButton jButton = (JButton) button;
+			jButton.setEnabled(false);
+		}
+	}
+}
+}
